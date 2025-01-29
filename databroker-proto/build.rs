4170313aa5510a5e3ceb8ae081e06c075a5fe3b2
lukasmittag <lukas.mittag@de.bosch.com>
2025-01-29 16:11:49 +0100

    Try out databroker-proto tonic version 0.12.3 with kuksa-perf

--- databroker-proto/build.rs
+++ databroker-proto/build.rs
@@ -18,7 +18,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .compile_well_known_types(false)
         .protoc_arg("--experimental_allow_proto3_optional")
-        .compile(
+        .compile_protos(
             &[
                 "proto/sdv/databroker/v1/broker.proto",
                 "proto/sdv/databroker/v1/types.proto",
@@ -34,7 +34,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
         .file_descriptor_set_path(out_dir.join("kuksa.val.v2_descriptor.bin"))
-        .compile(
+        .compile_protos(
             &[
                 "proto/kuksa/val/v2/val.proto",
                 "proto/kuksa/val/v2/types.proto",
